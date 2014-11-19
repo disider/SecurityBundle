cc0b03ec530dfd481bda71de5173996b01d58432
Neil Armstrong - Mac3 <neil@di-side.com>
2014-11-19 15:36:12 +0100

    Fixed all tests

--- Tests/cache/test/TestsTestDebugProjectContainer.php
+++ Tests/cache/test/TestsTestDebugProjectContainer.php
@@ -654,7 +654,7 @@ class TestsTestDebugProjectContainer extends Container
         $c = new \Doctrine\Common\Cache\ArrayCache();
         $c->setNamespace('sf2orm_default_468f8edbb8066e59ec4ccdaaf5bbd41f1269fc7c3e59bc042101a0535a8ad072');
 
-        $d = new \Doctrine\ORM\Mapping\Driver\SimplifiedXmlDriver(array('/git/projects/whalist/webapps/main/symfony/vendor/diside/security-bundle/Diside/SecurityBundle/Resources/config/doctrine' => 'Diside\\SecurityBundle\\Entity'));
+        $d = new \Doctrine\ORM\Mapping\Driver\SimplifiedXmlDriver(array('/git/projects/whalist/webapps/main/symfony/vendor/diside/security-bundle/Diside/SecurityBundle/Tests/config/doctrine' => 'Diside\\SecurityBundle\\Entity'));
         $d->setGlobalBasename('mapping');
 
         $e = new \Doctrine\ORM\Mapping\Driver\DriverChain();
@@ -1690,7 +1690,7 @@ class TestsTestDebugProjectContainer extends Container
         $m = new \Symfony\Component\Security\Http\Firewall\UsernamePasswordFormAuthenticationListener($a, $f, new \Symfony\Component\Security\Http\Session\SessionAuthenticationStrategy('migrate'), $i, 'main', $l, new \Symfony\Component\Security\Http\Authentication\DefaultAuthenticationFailureHandler($e, $i, array('login_path' => 'login', 'failure_path' => NULL, 'failure_forward' => false, 'failure_path_parameter' => '_failure_path'), NULL), array('check_path' => 'login_check', 'use_forward' => false, 'require_previous_session' => true, 'username_parameter' => '_username', 'password_parameter' => '_password', 'csrf_parameter' => '_csrf_token', 'intention' => 'authenticate', 'post_only' => true), NULL, $c, NULL);
         $m->setRememberMeServices($j);
 
-        return $this->services['security.firewall.map.context.main'] = new \Symfony\Bundle\SecurityBundle\Security\FirewallContext(array(0 => new \Symfony\Component\Security\Http\Firewall\ChannelListener($h, new \Symfony\Component\Security\Http\EntryPoint\RetryAuthenticationEntryPoint(80, 443), NULL), 1 => new \Symfony\Component\Security\Http\Firewall\ContextListener($a, array(0 => $b), 'main', NULL, $c), 2 => $k, 3 => $m, 4 => new \Symfony\Component\Security\Http\Firewall\RememberMeListener($a, $j, $f, NULL, $c), 5 => new \Symfony\Component\Security\Http\Firewall\AnonymousAuthenticationListener($a, '546ca0ab86475', NULL), 6 => new \Symfony\Component\Security\Http\Firewall\AccessListener($a, $g, $h, $f), 7 => new \Symfony\Component\Security\Http\Firewall\SwitchUserListener($a, $b, $this->get('security.user_checker'), 'main', $g, NULL, '_switch_user', 'ROLE_ALLOWED_TO_SWITCH', $c)), new \Symfony\Component\Security\Http\Firewall\ExceptionListener($a, $this->get('security.authentication.trust_resolver'), $i, 'main', new \Symfony\Component\Security\Http\EntryPoint\FormAuthenticationEntryPoint($e, $i, 'login', false), NULL, NULL, NULL));
+        return $this->services['security.firewall.map.context.main'] = new \Symfony\Bundle\SecurityBundle\Security\FirewallContext(array(0 => new \Symfony\Component\Security\Http\Firewall\ChannelListener($h, new \Symfony\Component\Security\Http\EntryPoint\RetryAuthenticationEntryPoint(80, 443), NULL), 1 => new \Symfony\Component\Security\Http\Firewall\ContextListener($a, array(0 => $b), 'main', NULL, $c), 2 => $k, 3 => $m, 4 => new \Symfony\Component\Security\Http\Firewall\RememberMeListener($a, $j, $f, NULL, $c), 5 => new \Symfony\Component\Security\Http\Firewall\AnonymousAuthenticationListener($a, '546caaba15d3f', NULL), 6 => new \Symfony\Component\Security\Http\Firewall\AccessListener($a, $g, $h, $f), 7 => new \Symfony\Component\Security\Http\Firewall\SwitchUserListener($a, $b, $this->get('security.user_checker'), 'main', $g, NULL, '_switch_user', 'ROLE_ALLOWED_TO_SWITCH', $c)), new \Symfony\Component\Security\Http\Firewall\ExceptionListener($a, $this->get('security.authentication.trust_resolver'), $i, 'main', new \Symfony\Component\Security\Http\EntryPoint\FormAuthenticationEntryPoint($e, $i, 'login', false), NULL, NULL, NULL));
     }
 
     /**
@@ -2911,7 +2911,7 @@ class TestsTestDebugProjectContainer extends Container
     {
         $a = $this->get('security.user_checker');
 
-        $this->services['security.authentication.manager'] = $instance = new \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager(array(0 => new \Symfony\Component\Security\Core\Authentication\Provider\DaoAuthenticationProvider($this->get('diside.security.user_provider'), $a, 'main', $this->get('security.encoder_factory'), true), 1 => new \Symfony\Component\Security\Core\Authentication\Provider\RememberMeAuthenticationProvider($a, 'ThisTokenIsNotSoSecretChangeIt', 'main'), 2 => new \Symfony\Component\Security\Core\Authentication\Provider\AnonymousAuthenticationProvider('546ca0ab86475')), true);
+        $this->services['security.authentication.manager'] = $instance = new \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager(array(0 => new \Symfony\Component\Security\Core\Authentication\Provider\DaoAuthenticationProvider($this->get('diside.security.user_provider'), $a, 'main', $this->get('security.encoder_factory'), true), 1 => new \Symfony\Component\Security\Core\Authentication\Provider\RememberMeAuthenticationProvider($a, 'ThisTokenIsNotSoSecretChangeIt', 'main'), 2 => new \Symfony\Component\Security\Core\Authentication\Provider\AnonymousAuthenticationProvider('546caaba15d3f')), true);
 
         $instance->setEventDispatcher($this->get('debug.event_dispatcher'));
 
@@ -3487,16 +3487,16 @@ class TestsTestDebugProjectContainer extends Container
             'sensio_framework_extra.converter.doctrine.class' => 'Sensio\\Bundle\\FrameworkExtraBundle\\Request\\ParamConverter\\DoctrineParamConverter',
             'sensio_framework_extra.converter.datetime.class' => 'Sensio\\Bundle\\FrameworkExtraBundle\\Request\\ParamConverter\\DateTimeParamConverter',
             'sensio_framework_extra.view.listener.class' => 'Sensio\\Bundle\\FrameworkExtraBundle\\EventListener\\TemplateListener',
-            'diside.gateway.gateway_register.class' => 'Diside\SecurityComponent\\Gateway\\GatewayRegister',
+            'diside.gateway.gateway_register.class' => 'Diside\\SecurityComponent\\Gateway\\GatewayRegister',
             'diside.security.gateway.log_gateway.class' => 'Diside\\SecurityBundle\\Gateway\\ORM\\ORMLogGateway',
             'diside.security.gateway.company_gateway.class' => 'Diside\\SecurityBundle\\Gateway\\ORM\\ORMCompanyGateway',
             'diside.security.gateway.user_gateway.class' => 'Diside\\SecurityBundle\\Gateway\\ORM\\ORMUserGateway',
-            'diside.interactor.security_interactor_register.class' => 'Diside\SecurityComponent\\Interactor\\SecurityInteractorRegister',
-            'diside.interactor.interactor_factory.class' => 'Diside\SecurityComponent\\Interactor\\InteractorFactory',
+            'diside.interactor.security_interactor_register.class' => 'Diside\\SecurityComponent\\Interactor\\SecurityInteractorRegister',
+            'diside.interactor.interactor_factory.class' => 'Diside\\SecurityComponent\\Interactor\\InteractorFactory',
             'diside.security.security.voter.role_voter.class' => 'Diside\\SecurityBundle\\Security\\Voter\\RoleVoter',
             'diside.security.security.voter.user_voter.class' => 'Diside\\SecurityBundle\\Security\\Voter\\UserVoter',
             'diside.security.user_provider.class' => 'Diside\\SecurityBundle\\Security\\UserProvider',
-            'security.logger.logger.class' => 'Diside\SecurityComponent\\Logger\\Logger',
+            'security.logger.logger.class' => 'Diside\\SecurityComponent\\Logger\\Logger',
             'security.form.processor.company_form_processor.class' => 'Diside\\SecurityBundle\\Form\\Processor\\CompanyFormProcessor',
             'security.form.processor.user_form_processor.class' => 'Diside\\SecurityBundle\\Form\\Processor\\UserFormProcessor',
             'security.form.processor.category_form_processor.class' => 'Diside\\SecurityBundle\\Form\\Processor\\CategoryFormProcessor',
